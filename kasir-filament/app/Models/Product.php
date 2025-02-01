27fbbab4053ba043e234a5e5ea66cea2aba4f903
ivanmuntahir <77213038+ivanmuntahir@users.noreply.github.com>
2025-02-01 23:30:50 +0700

    add product resource, add product cluster, fix relationship Product and Category

--- kasir-filament/app/Models/Product.php
+++ kasir-filament/app/Models/Product.php
@@ -5,12 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
 
-    public function products(): BelongsTo{
-        return $this->belongsTo(Product::class);
+    protected $fillable = [
+        'name',
+        'slug',
+        'category_id',
+        'stock',
+        'price',
+        'is_active',
+        'image',
+        'barcode',
+        'description',
+    ];
+
+    public function category(): BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    public static function generateUniqueSlug(string $name): string {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while(self::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
     }
 }
