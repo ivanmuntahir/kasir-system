5eb1ad1394379679b6f2702bb42352ae151bde02
ivanmuntahir <77213038+ivanmuntahir@users.noreply.github.com>
2025-02-01 23:06:55 +0700

    add function generateUniqueSlug

--- kasir-filament/app/Models/Category.php
+++ kasir-filament/app/Models/Category.php
@@ -5,12 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'is_active',
+    ];
+
     public function products(): HasMany{
         return $this->hasMany(Product::class);
+    }
+
+    public static function generateUniqueSlug(string $name): string {
+        $slug = Str::slug($name);
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while(self::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+
+        return $slug;
     }
 }
